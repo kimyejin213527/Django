62e3dcaf7ffcc5b401932120c03e3bc4b0855f59
yejin <yejin213527@gmail.com>
2023-03-22 10:13:05 +0900

    urls,views,templates playground/hello_html/ -> say_hello_html() -> playground/hello.html

--- playground/views.py
+++ playground/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def say_hello(request):
     return HttpResponse("Hello, world!")
+
+
+def say_hello_html(request):
+    return render(request, "playground/hello.html")
